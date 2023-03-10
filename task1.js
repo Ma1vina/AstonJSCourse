623cb79caabf072fc72cc199a7c801d1f3d39689
Ma1vina <chekmeneva.marinna@mail.ru>
2023-03-10 16:20:54 +0300

    Errors correction

--- task1.js
+++ task1.js
@@ -2,9 +2,8 @@ Array.prototype.filterArray = function (cb, thisArg) {
   let context = this;
   const res = [];
   for (let i = 0; i < context.length; i++) {
-    if (cb.call(thisArg, context[i], i, context)) {
+    if (!cb.call(thisArg, context[i], i, context)) continue;
       res.push(context[i]);
-    }
   }
   return res;
 };
